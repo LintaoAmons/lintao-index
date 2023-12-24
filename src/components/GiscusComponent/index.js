5b590048efb7bc90951ea04dba964857ea9024af
Lintao <lintao.amons@outlook.com>
2023-12-24 16:46:35 +0800

    bringback comments

---
+++ src/components/GiscusComponent/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Giscus from "@giscus/react";
+import { useColorMode } from "@docusaurus/theme-common";
+import { useLocation } from "@docusaurus/router";
+
+export default function GiscusComponent() {
+  const { colorMode } = useColorMode();
+  const { pathname } = useLocation();
+
+  return (
+    <Giscus
+      repo="LintaoAmons/lintao-index"
+      repoId="R_kgDOKcy8Bw"
+      category="General"
+      categoryId="DIC_kwDOKcy8B84CZ71z" // E.g. id of "General"
+      mapping="url" // Important! To map comments to URL
+      term="Welcome to @giscus/react component!"
+      strict="0"
+      reactionsEnabled="1"
+      emitMetadata="1"
+      inputPosition="top"
+      theme={colorMode}
+      lang="en"
+      loading="lazy"
+      crossorigin="anonymous"
+      async
+    />
+  );
+}
